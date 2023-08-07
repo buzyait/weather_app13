d0110b06b9db9d94b37b878d7e3f9e4b858b11f5
Zeynefly <zeyne.muktarieva.ch@gmail.com>
2023-08-07 16:59:20 +0300

    weather

--- lib/constants/api_const.dart
+++ lib/constants/api_const.dart
@@ -1,8 +1,13 @@
 class ApiConst {
-  static const String apiKey = 'edeec702d15a4f2cd857e9917031d215';
+  static const String apiKey = '8399a11d4cc690469e5d3c2caa02dc98';
   static const String cityName = 'moscow';
-  static const String api =
-      'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=edeec702d15a4f2cd857e9917031d215';
+  static String api({String? cityName}) =>
+      'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey';
+
+  static String getLocator({double? lat, double? lon}) {
+    return 'https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&appid=$apiKey';
+  }
+
   static String getIcon(String icon, int size) {
     return 'http://openweathermap.org/img/wn/$icon@${size}x.png';
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,47 +1,87 @@
-import 'dart:math';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:weather_app13/components/custom_icon_button.dart';
 import 'package:weather_app13/constants/api_const.dart';
+import 'package:weather_app13/constants/app_colrs.dart';
 import 'package:weather_app13/constants/app_text.dart';
 import 'package:weather_app13/models/weather_model.dart';
 
-class HomePage extends StatelessWidget {
+List<String> cities = [
+  'bishkek',
+  'osh',
+  'talas',
+  'jalal-abad',
+  'batken',
+  'tokmok',
+];
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  Future<Position> weatherLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
+class _HomePageState extends State<HomePage> {
+  WeatherModel? weatherModel;
+  Future<void> weatherLocation() async {
+    setState(() {
+      weatherModel = null;
+    });
+    LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
-
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
+      if (permission == LocationPermission.always &&
+          permission == LocationPermission.whileInUse) {
+        Position position = await Geolocator.getCurrentPosition();
+        final dio = Dio();
+        final response = await dio.get(ApiConst.getLocator(
+          lat: position.latitude,
+          lon: position.longitude,
+        ));
+        if (response.statusCode == 200 || response.statusCode == 201) {
+          weatherModel = WeatherModel(
+            id: response.data['current']['weather'][0]['id'],
+            main: response.data['current']['weather'][0]['main'],
+            description: response.data['current']['weather'][0]['description'],
+            icon: response.data['current']['weather'][0]['icon'],
+            temp: response.data['current']['temp'],
+            country: response.data['timezone'],
+            city: response.data['timezone'],
+          );
+        }
+        setState(() {});
       }
+    } else {
+      Position position = await Geolocator.getCurrentPosition();
+      final dio = Dio();
+      final response = await dio.get(ApiConst.getLocator(
+        lat: position.latitude,
+        lon: position.longitude,
+      ));
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        weatherModel = WeatherModel(
+          id: response.data['current']['weather'][0]['id'],
+          main: response.data['current']['weather'][0]['main'],
+          description: response.data['current']['weather'][0]['description'],
+          icon: response.data['current']['weather'][0]['icon'],
+          temp: response.data['current']['temp'],
+          country: response.data['timezone'],
+          city: response.data['timezone'],
+        );
+      }
+      setState(() {});
     }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-    return await Geolocator.getCurrentPosition();
   }
 
-  Future<WeatherModel?> fetchData() async {
+  Future<void> weatherName({String? cityName}) async {
     final dio = Dio();
-    final response = await dio.get(ApiConst.api);
+    final response = await dio.get(
+      ApiConst.api(cityName: cityName ?? 'Bishkek'),
+    );
     if (response.statusCode == 200 || response.statusCode == 201) {
-      final weatherModel = WeatherModel(
+      weatherModel = WeatherModel(
         id: response.data['weather'][0]['id'],
         main: response.data['weather'][0]['main'],
         description: response.data['weather'][0]['description'],
@@ -50,10 +90,15 @@ class HomePage extends StatelessWidget {
         country: response.data['sys']['country'],
         city: response.data['name'],
       );
-      //setState(() {});
-      return weatherModel;
     }
-    return null;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    weatherName();
+
+    super.initState();
   }
 
   @override
@@ -71,112 +116,132 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: FutureBuilder(
-          future: fetchData(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.connectionState == ConnectionState.none) {
-              return const Center(
-                child: Text('Internet bailanyshynda uzgultuk bar'),
-              );
-              //connectionState -danniydyn abaly
-            } else if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasData) {
-                return Container(
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/img.jpg'),
-                      fit: BoxFit.fill,
+      body: weatherModel == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/img.jpg'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomIconsButton(
+                          onPressed: () async {
+                            await weatherLocation();
+                          },
+                          icon: Icons.near_me),
+                      CustomIconsButton(
+                          onPressed: () {
+                            showBottomSheet();
+                          },
+                          icon: Icons.location_city),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Expanded(
+                    flex: 2,
+                    child: Row(
+                      children: [
+                        SizedBox(width: 12),
+                        Text(
+                          '${temp(weatherModel!.temp!)}',
+                          // kachan gana ishenimduu kelet deser ilep belgi koulat pagoda misaly
+                          style: const TextStyle(
+                            fontSize: 36,
+                            color: Color(0xff000000),
+                          ),
+                        ),
+                        Image.network(ApiConst.getIcon(weatherModel!.icon, 4)),
+                      ],
                     ),
                   ),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          CustomIconsButton(
-                              onPressed: () async {
-                                weatherLocation();
-                              },
-                              icon: Icons.near_me),
-                          const CustomIconsButton(icon: Icons.location_city),
-                        ],
-                      ),
-                      const SizedBox(height: 10),
-                      Expanded(
-                        flex: 2,
-                        child: Row(
-                          children: [
-                            SizedBox(width: 12),
-                            Text(
-                              '${temp(snapshot.data!.temp!)}',
-                              // kachan gana ishenimduu kelet deser ilep belgi koulat pagoda misaly
-                              style: const TextStyle(
-                                fontSize: 36,
-                                color: Color(0xff000000),
-                              ),
-                            ),
-                            Image.network(
-                                ApiConst.getIcon(snapshot.data!.icon, 4)),
-                          ],
+                  Expanded(
+                    flex: 2,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          weatherModel!.description.replaceAll(' ', '\n'),
+                          style: const TextStyle(
+                            fontSize: 60,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              snapshot.data!.description.replaceAll(' ', '\n'),
-                              style: const TextStyle(
-                                fontSize: 60,
-                                color: Colors.white,
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 40,
-                            )
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      Expanded(
-                        flex: 1,
-                        child: Align(
-                          alignment: Alignment.bottomRight,
-                          child: Padding(
-                            padding: const EdgeInsets.only(right: 40),
-                            child: Text(
-                              snapshot.data!.city,
-                              style: const TextStyle(
-                                fontSize: 42,
-                                color: Colors.white,
-                              ),
-                            ),
+                        const SizedBox(
+                          width: 40,
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Expanded(
+                    flex: 1,
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 40),
+                        child: Text(
+                          weatherModel!.city,
+                          style: const TextStyle(
+                            fontSize: 42,
+                            color: Colors.white,
                           ),
                         ),
                       ),
-                    ],
+                    ),
                   ),
-                );
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text(snapshot.error.toString()),
-                );
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            } else {
-              return const Center(child: Text('Belgisiz kata bar...'));
-            }
-          }),
+                ],
+              ),
+            ),
     );
   }
 
   int temp(double temp) {
     return (temp - 273.15).toInt();
   }
-  //butun bolugun 4ygaryp beret bul logika
+
+  void showBottomSheet() {
+    showModalBottomSheet<void>(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          decoration: BoxDecoration(
+              border: Border.all(color: AppColors.white),
+              color: Colors.black26,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              )),
+          height: MediaQuery.of(context).size.height * 0.6,
+          child: ListView.builder(
+              itemCount: cities.length,
+              itemBuilder: (context, index) {
+                final city = cities[index];
+                return Card(
+                  child: ListTile(
+                    onTap: () {
+                      setState(() {
+                        weatherModel = null;
+                      });
+                      weatherName(cityName: city);
+                      Navigator.pop(context);
+                    },
+                    title: Text(city),
+                  ),
+                );
+              }),
+        );
+      },
+    );
+  }
 }
